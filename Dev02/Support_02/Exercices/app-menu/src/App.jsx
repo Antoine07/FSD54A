b6d8d4056d403a6d8ff67d5e3475e95fb4b7673f
Antoine07 <antoine.lucsko@gmail.com>
2024-10-01 15:35:41 +0200

    ma

--- Dev02/Support_02/Exercices/app-menu/src/App.jsx
+++ Dev02/Support_02/Exercices/app-menu/src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import NotFound from './components/NotFound'
+import Layout from './components/Layout'
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <h1>Hello</h1>       
-    </>
+    <Router>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
+  </Router>
   )
 }
 
