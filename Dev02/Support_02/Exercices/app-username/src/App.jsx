75e7f2bff0724ab32bd8d1790fc152c63ddd30a0
Antoine07 <antoine.lucsko@gmail.com>
2024-10-03 16:23:52 +0200

    app username

--- Dev02/Support_02/Exercices/app-username/src/App.jsx
+++ Dev02/Support_02/Exercices/app-username/src/App.jsx
@@ -2,10 +2,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import './App.css'
 import Form from './components/Form'
 import { lenName } from './features/lenNameSlice'
+import { orderName, deletName } from './features/nameSlice'
 
 function App() {
   // useSelector permet de lire le state qui se trouve dans vos createSlice
-  const { names } = useSelector(state => state.name)
+  const { names, sens } = useSelector(state => state.name)
   const { len, pos } = useSelector(state => state.lenName)
   const dispatch = useDispatch()
 
@@ -13,18 +14,30 @@ function App() {
     dispatch(lenName({ name, index }))
   }
 
+  const handleDelete = index => {
+    dispatch(deletName(index))
+  }
+
   return (
     <>
       <Form />
+      <button onClick={() => dispatch(orderName(sens))}>
+        Ordre {sens > 0 ? 'croissant' : 'décroissant'}
+      </button>
       <ul style={{ listStyle: 'none' }}>
         {names.map((name, index) => <li key={index}>
           {name}
           <br />
-          <button 
-            className='btn' 
+          <button
+            className='btn'
             onClick={() => handleLength(name, index)}
           >length Name</button>
           {pos === index && <small> {len} </small>}
+          <button
+            className='btn'
+            style={{color : 'red'}}
+            onClick={() => handleDelete(index)}
+          >delete</button>
         </li>)}
       </ul>
 
--- Dev02/Support_02/Exercices/app-username/src/features/nameSlice.jsx
+++ Dev02/Support_02/Exercices/app-username/src/features/nameSlice.jsx
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const nameSlice = createSlice({
     name: 'names',
-    initialState: { names: [], message : '' },
+    initialState: { names: [], message : '', sens : 1 },
     reducers: {
         addName: (state, action) => {
             state.message = ''
@@ -14,12 +14,21 @@ const nameSlice = createSlice({
         },
         setMessage : (state, action) => {
             state.message = action.payload
+        },
+        orderName : (state, action) =>{
+            const s = Math.abs( action.payload ) === 1 ? action.payload : action.payload
+            state.sens = -1 * s
+            state.names.sort((a, b) => s * a.localeCompare(b))
+        },
+        deletName : (state, action) =>{
+            const index = action.payload
+            state.names.splice(index, 1)
         }
     },
 });
 
 // Exporter les actions générées par le slice
-export const { addName, setMessage } = nameSlice.actions
+export const { addName, setMessage, orderName, deletName } = nameSlice.actions
 
 // Exporter le reducer pour l'intégrer dans le store
 export default nameSlice.reducer
